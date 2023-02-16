5aa57d0d3f61a7915a3174bd17f08f42f1a0fefb
Jeroen <j.schut@students.uu.nl>
2023-02-16 15:04:50 +0100

    Help

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import numpy as np
 import glob
 
 # Global Variables
-SQUARE_SIZE: int = 24
+SQUARE_SIZE: float = 24.23
 WIDTH: int = 9
 HEIGHT: int = 6
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+chessBoardFlags = cv.CALIB_CB_ADAPTIVE_THRESH + cv.CALIB_CB_FAST_CHECK + cv.CALIB_CB_NORMALIZE_IMAGE
 
 corners3 = []
 
@@ -30,6 +31,7 @@ def runCalibration():
     for index, obj in enumerate(objp):
         objp[index] = obj * SQUARE_SIZE
 
+
     # Arrays to store object points and image points from all the images.
     objpoints = []  # 3d point in real world space
     imgpoints = []  # 2d points in image plane.
@@ -47,7 +49,7 @@ def runCalibration():
         cv.imshow('img', img)
 
         # Try to find corners
-        success, corners = cv.findChessboardCorners(grayImg, (WIDTH, HEIGHT), None)
+        success, corners = cv.findChessboardCorners(grayImg, (WIDTH, HEIGHT), chessBoardFlags)
 
         # If successfully found
         if True:
@@ -94,9 +96,6 @@ def runCalibration():
     ret, matrix, distortion, r_vecs, t_vecs = cv.calibrateCamera(objpoints, imgpoints, grayImg.shape[:2], None, None)
 
 def draw(frame):
-    # Read frame
-    img = cv.imread(frame)
-
     # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
     objp = np.zeros((WIDTH * HEIGHT, 3), np.float32)
     objp[:, :2] = np.mgrid[0:HEIGHT, 0:WIDTH].T.reshape(-1, 2)
@@ -105,34 +104,46 @@ def draw(frame):
         objp[index] = obj * SQUARE_SIZE
 
     # Turn image to grayscale
-    grayImg = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    grayImg = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
     # Show image
-    cv.imshow('img', img)
+    cv.imshow('img', frame)
 
     # Try to find corners
-    success, corners = cv.findChessboardCorners(grayImg, (WIDTH, HEIGHT), None)
+    success, corners = cv.findChessboardCorners(grayImg, (WIDTH, HEIGHT), chessBoardFlags)
 
     if(success):
         corners2 = cv.cornerSubPix(grayImg, corners, (11, 11), (-1, -1), criteria)
 
-        rvecs, tvecs = cv.solvePnP(objp, corners2, matrix, distortion)
+        ret, rvecs, tvecs = cv.solvePnP(objp, corners2, matrix, distortion)
 
-        frame = drawAxis(frame,  rvecs, tvecs)
-        #frame = drawCube(frame, rvecs, tvecs)
+        finalImg = drawAxis(frame, corners2[0], rvecs, tvecs)
+        #frame = drawCube(img, rvecs, tvecs)
 
-def drawAxis(frame, rvecs, tvecs):
-    axis = np.float32([[3,0,0] * SQUARE_SIZE, [0,3,0] * SQUARE_SIZE, [0,0,-3] * SQUARE_SIZE, [0,0,0]]).reshape(-1,4)
+        cv.imshow('img', finalImg)
+        cv.waitKey(0)
 
+def drawAxis(frame, corner, rvecs, tvecs):
+    axis = np.float32([[3 * SQUARE_SIZE,0,0], [0,3 * SQUARE_SIZE,0], [0,0,-3 * SQUARE_SIZE], [0,0,0]]).reshape(-1,3)
+    print(axis)
     imgpts, jac = cv.projectPoints(axis, rvecs, tvecs, matrix, distortion)
 
+    print(matrix)
+    print(distortion)
+
+    origin = tuple(map(int,(imgpts[3].ravel())))
     print(imgpts)
+    cv.line(frame, origin, tuple(map(int,(imgpts[0].ravel()))), (255, 0, 0), 3)
+    cv.line(frame, origin, tuple(map(int,(imgpts[1].ravel()))), (0, 255, 0), 3)
+    cv.line(frame, origin, tuple(map(int,(imgpts[2].ravel()))), (0, 0, 255), 3)
+
+    return frame
 
 
 runCalibration()
 
 
-images = glob.glob('images/*.jpg')
-draw(images[0])
+frame = cv.imread("images/WIN_20220216_15_16_03_Pro.jpg");
+draw(frame)
 
 cv.destroyAllWindows()
