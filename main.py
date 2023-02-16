edf1b25954de8a2e543af4628f58ab95eea5574c
Gideon Ogilvie <g.a.ogilvie@students.uu.nl>
2023-02-16 20:02:13 +0100

    fixed drawing

--- main.py
+++ main.py
@@ -2,6 +2,24 @@ import cv2 as cv
 import numpy as np
 import glob
 
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
+
+    def __truediv__(self, other):
+        return Point(self.x / other, self.y / other)
+
+    def __mul__(self, other):
+        return Point(self.x * other, self.y * other)
+
+
 # Global Variables
 SQUARE_SIZE: float = 24.23
 WIDTH: int = 6
@@ -11,17 +29,20 @@ chessBoardFlags = cv.CALIB_CB_ADAPTIVE_THRESH + cv.CALIB_CB_FAST_CHECK + cv.CALI
 
 corners3 = []
 
+
 def click_event(event, x, y, flags, params):
     global corners3
     if event == cv.EVENT_LBUTTONDOWN:
         corners3.append([x, y])
+
 
 def interpolate(i, x, y):
     x1 = x.x
     x2 = y.x
     y1 = x.y
     y2 = y.y
-    return y1 + (i - x1)((y2-y1)/(x2-x1))
+    return y1 + (i - x1)((y2 - y1) / (x2 - x1))
+
 
 def runCalibration():
     # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
@@ -31,14 +52,13 @@ def runCalibration():
     for index, obj in enumerate(objp):
         objp[index] = obj * SQUARE_SIZE
 
-
     # Arrays to store object points and image points from all the images.
     objpoints = []  # 3d point in real world space
     imgpoints = []  # 2d points in image plane.
 
     images = glob.glob('images/*.jpg')
 
-    for (index,fname) in enumerate(images):
+    for (index, fname) in enumerate(images):
         # Get Image
         img = cv.imread(fname)
 
@@ -83,37 +103,35 @@ def runCalibration():
             # TODO: interpolate
 
             # Increase corner accuracy and append
-            asdf = []
-            topRightX = corners3[0][0] #top right x
-            topRightY = corners3[0][1]
-            topLeftX = corners3[1][0]
-            topLeftY = corners3[1][1]
-            bottomRightX = corners3[2][0]
-            bottomRightY = corners3[2][1]
-            bottomLeftX = corners3[3][0]
-            bottomLeftY = corners3[3][1]
+            boardPoints = []
+            tR = Point(corners3[0][0], corners3[0][1])
+            tL = Point(corners3[1][0], corners3[1][1])
+            bR = Point(corners3[2][0], corners3[2][1])
+            bL = Point(corners3[3][0], corners3[3][1])
 
-            deltax = (abs(topRightX - topLeftX) / 8.0, abs(topRightY - topLeftY) / 8)
-            deltay = (abs(topRightX - bottomRightX) / 5, abs(topRightY - bottomRightY) / 5)
+            tRtobR = tR - bR
+            tLtobL = tL - bL
+            tRtobRd = tRtobR / (WIDTH - 1)
+            tLtobLd = tLtobL / (WIDTH - 1)
 
-            #deltaYRight = (abs(topRightX - bottomRightX) / 5, abs(topRightY - bottomRightY) / 5)
-            #deltaYLeft = (abs(topLeftX - bottomLeftX) / 5, abs(topLeftY - bottomRightY) / 5)
-
-            for i in range(HEIGHT):
-
-                for j in range(WIDTH):
-                    asdf.append((corners3[0][0] + -i * deltax[0] + -j * deltay[0], corners3[0][1] + i * deltax[1] + j * deltay[1]))
+            for i in range(WIDTH):
+                start = tR - tRtobRd * i
+                end = tL - tLtobLd * i
+                step = (end - start) / (HEIGHT - 1)
+                for j in range(HEIGHT):
+                    res = start + step * j
+                    boardPoints.append((res.x, res.y))
 
             objpoints.append(objp)
-            asdf = list(map(lambda x : (np.float32(x[0]), np.float32(x[1])),asdf))
-            corners4 = np.array(asdf).reshape(54,1,2)
-            #print(corners4)
-            #corners5 = cv.cornerSubPix(grayImg, corners4, (11, 11), (-1, -1), criteria)
+            boardPoints = list(map(lambda x: (np.float32(x[0]), np.float32(x[1])), boardPoints))
+            corners4 = np.array(boardPoints).reshape(54, 1, 2)
+            # print(corners4)
+            # corners5 = cv.cornerSubPix(grayImg, corners4, (11, 11), (-1, -1), criteria)
             imgpoints.append(corners4)
 
             # Draw and display the corners
-            #for i in range(1,54):
-                #cv.line(img, tuple(map(int, asdf[i-1])), tuple(map(int, asdf[i])), ((i%5) * 50, 255-(i%5) * 50, 0), 3)
+            # for i in range(1,54):
+            # cv.line(img, tuple(map(int, asdf[i-1])), tuple(map(int, asdf[i])), ((i%5) * 50, 255-(i%5) * 50, 0), 3)
 
             print(corners4)
             cv.drawChessboardCorners(img, (WIDTH, HEIGHT), corners4, True)
@@ -123,6 +141,7 @@ def runCalibration():
 
     global ret, matrix, distortion
     ret, matrix, distortion, _, _ = cv.calibrateCamera(objpoints, imgpoints, grayImg.shape[::-1], None, None)
+
 
 def draw(frame):
     # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
@@ -141,7 +160,7 @@ def draw(frame):
     # Try to find corners
     success, corners = cv.findChessboardCorners(grayImg, (WIDTH, HEIGHT), chessBoardFlags)
 
-    if(success):
+    if (success):
         corners2 = cv.cornerSubPix(grayImg, corners, (11, 11), (-1, -1), criteria)
 
         ret, rvecs, tvecs = cv.solvePnP(objp, corners2, matrix, distortion)
@@ -152,29 +171,34 @@ def draw(frame):
         cv.imshow('img', finalImg)
         cv.waitKey(0)
 
+
 def drawAxis(frame, rvecs, tvecs):
-    axis = np.float32([[3 * SQUARE_SIZE,0,0], [0,3 * SQUARE_SIZE,0], [0,0,-3 * SQUARE_SIZE], [0,0,0]]).reshape(-1,3)
+    axis = np.float32([[3 * SQUARE_SIZE, 0, 0], [0, 3 * SQUARE_SIZE, 0], [0, 0, -3 * SQUARE_SIZE], [0, 0, 0]]).reshape(
+        -1, 3)
 
     imgpts, jac = cv.projectPoints(axis, rvecs, tvecs, matrix, np.zeros(5))
 
-    origin = tuple(map(int,(imgpts[3].ravel())))
+    origin = tuple(map(int, (imgpts[3].ravel())))
 
-    cv.line(frame, origin, tuple(map(int,(imgpts[0].ravel()))), (255, 0, 0), 3)
-    cv.line(frame, origin, tuple(map(int,(imgpts[1].ravel()))), (0, 255, 0), 3)
-    cv.line(frame, origin, tuple(map(int,(imgpts[2].ravel()))), (0, 0, 255), 3)
+    cv.line(frame, origin, tuple(map(int, (imgpts[0].ravel()))), (255, 0, 0), 3)
+    cv.line(frame, origin, tuple(map(int, (imgpts[1].ravel()))), (0, 255, 0), 3)
+    cv.line(frame, origin, tuple(map(int, (imgpts[2].ravel()))), (0, 0, 255), 3)
 
     return frame
 
+
 def drawCube(frame, rvecs, tvecs):
     vec = 2 * SQUARE_SIZE
-    axis = np.float32([[0, vec, 0], [vec, vec, 0], [vec, 0, 0], [0, 0, -vec], [0, vec, -vec], [vec, vec, -vec], [vec, 0, -vec], [0, 0, 0]]).reshape(
+    axis = np.float32(
+        [[0, vec, 0], [vec, vec, 0], [vec, 0, 0], [0, 0, -vec], [0, vec, -vec], [vec, vec, -vec], [vec, 0, -vec],
+         [0, 0, 0]]).reshape(
         -1, 3)
 
     imgpts, jac = cv.projectPoints(axis, rvecs, tvecs, matrix, distortion)
 
     origin = tuple(map(int, (imgpts[7].ravel())))
 
-    color = (255, 0 , 255)
+    color = (255, 0, 255)
 
     cv.line(frame, origin, tuple(map(int, (imgpts[0].ravel()))), color, 3)
     cv.line(frame, origin, tuple(map(int, (imgpts[2].ravel()))), color, 3)
@@ -195,11 +219,11 @@ def drawCube(frame, rvecs, tvecs):
     cv.line(frame, tuple(map(int, (imgpts[5].ravel()))), tuple(map(int, (imgpts[1].ravel()))), color, 3)
 
 
-
 runCalibration()
-
 
 frame = cv.imread("images/WIN_20220216_15_16_03_Pro.jpg");
 draw(frame)
 
 cv.destroyAllWindows()
+
+
